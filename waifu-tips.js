6b2f686894384b096bd9c1796fdee3cf0eb8490f
yan <81685716+yan-930521@users.noreply.github.com>
2021-12-19 13:08:51 +0000

    renew

--- waifu-tips.js
+++ waifu-tips.js
@@ -39,7 +39,7 @@ function loadWidget(live2d_path, others_path) {
   let userAction = false,
     userActionTimer,
     messageTimer,
-    messageArray = ["好久不见，日子过得好快呢……", "大坏蛋！你都多久没理人家了呀，嘤嘤嘤～", "嗨～快来逗我玩吧！", "拿小拳拳锤你胸口！", "记得把小家加入 Adblock 白名单哦！"];
+    messageArray = ["好久不見啊！", "人家好無聊喔......"];
 
   window.addEventListener("mousemove", () => userAction = true);
   window.addEventListener("keydown", () => userAction = true);
@@ -59,17 +59,17 @@ function loadWidget(live2d_path, others_path) {
   (function registerEventListener() {
 
     document.querySelector("#waifu-tool .fa-camera-retro").addEventListener("click", () => {
-      showMessage("照好了嘛，是不是很可爱呢？", 6000, 9);
+      showMessage("人家可愛吧？", 6000, 9);
       Live2D.captureName = "photo.png";
       Live2D.captureFrame = true;
     });
 
     window.addEventListener("copy", () => {
-      showMessage("你都复制了些什么呀，转载要记得加上出处哦！", 6000, 9);
+      showMessage("hummm 你複製了什麼呢？", 6000, 9);
     });
 
     window.addEventListener("visibilitychange", () => {
-      if (!document.hidden) showMessage("哇，你终于回来了～", 6000, 9);
+      if (!document.hidden) showMessage("歡迎回家～", 6000, 9);
     });
 
   })();
@@ -78,14 +78,11 @@ function loadWidget(live2d_path, others_path) {
     let text;
     if (location.pathname === "/") { // 如果是主页
       const now = new Date().getHours();
-      if (now > 5 && now <= 7) text = "早上好！一日之计在于晨，美好的一天就要开始了。";
-      else if (now > 7 && now <= 11) text = "上午好！工作顺利嘛，不要久坐，多起来走动走动哦！";
-      else if (now > 11 && now <= 13) text = "中午了，工作了一个上午，现在是午餐时间！";
-      else if (now > 13 && now <= 17) text = "午后很容易犯困呢，今天的运动目标完成了吗？";
-      else if (now > 17 && now <= 19) text = "傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红～";
-      else if (now > 19 && now <= 21) text = "晚上好，今天过得怎么样？";
-      else if (now > 21 && now <= 23) text = ["已经这么晚了呀，早点休息吧，晚安～", "深夜时要爱护眼睛呀！"];
-      else text = "你是夜猫子呀？这么晚还不睡觉，明天起的来嘛？";
+      if (now > 5 && now <= 11) text = "早安呦！";
+      else if (now > 11 && now <= 17) text = "午安！ 吃午餐了嗎？";
+      else if (now > 17 && now <= 21) text = "晚安 今天過的怎麼樣呢？";
+      else if (now > 21 && now <= 23) text = "記得早一點睡喔！";
+      else text = "好晚了... 快點去睡覺啦...！";
     }
     showMessage(text, 7000, 8);
   })();
